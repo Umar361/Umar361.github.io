9f8d7a097adf29476706d6590512bcb4e31cef4c
Umar361 <umarjutt361@gmail.com>
2023-01-03 01:01:44 +0400

    serviceworker update

--- service-worker.js
+++ service-worker.js
@@ -6,10 +6,4 @@ importScripts(
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
   }
-  if (window.location.protocol === 'http:') {
-    const requireHTTPS = document.getElementById('requireHTTPS');
-    const link = requireHTTPS.querySelector('a');
-    link.href = window.location.href.replace('http://', 'https://');
-    requireHTTPS.classList.remove('hidden');
-  }
 
