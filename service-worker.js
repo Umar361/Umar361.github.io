92a19cb39f56f507b690c5e7d7e5a50a7a403146
umarshan <umar@infosun.co.uk>
2023-01-16 21:14:22 +0000

    fixes

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,40 @@
-// const CACHE_NAME = 'offline';
-// const OFFLINE_URL = 'offline.html';
-// importScripts(
-//     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-//   );
-//   if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('/service-worker.js');
-//   }
-
-var CACHE_NAME = 'pwgen-cache-v1';
+var CACHE_NAME = "cache-v1";
 var urlsToCache = [
-    './index.html',
-    './service-worker.js',
-    './manifest.json',
-    './assets/css/styling.css',
-    './assets/css/themify-icons.css',
-    './assets/css/virtual.css',
-    './assets/css/minibar.virtual.css',
+  "./index.html",
+  "./service-worker.js",
+  "./manifest.json",
+  "./assets/css/styling.css",
+  "./assets/css/themify-icons.css",
+  "./assets/css/virtual.css",
+  "./assets/css/minibar.virtual.css",
 ];
-console.log('loading sw');
+console.log("loading sw");
 
-self.addEventListener('install', function(event) {
-    // Perform install steps
-    console.log('installing sw');
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                try {
-                    console.log('Opened cache');
-                    var x = cache.addAll(urlsToCache);
-                    console.log('cache added');
-                    return x;
-                } catch (error) {
-                    return null
-                }
-                
-            })
-    );
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  console.log("installing sw");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      try {
+        console.log("Opened cache");
+        var x = cache.addAll(urlsToCache);
+        console.log("cache added");
+        return x;
+      } catch (error) {
+        return null;
+      }
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                    // Cache hit - return response
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
-                }
-            )
-    );
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
 });
--- tasks/converter/converter.js
+++ tasks/converter/converter.js
@@ -1,88 +1,71 @@
-const btn = document.querySelector('.btn');
-const selectFrom = document.querySelector('#select_from');
-const selectTo = document.querySelector('#select_to');
-const result = document.querySelector('.result');
-const result_par = document.querySelector('.result_par');
-let amount = document.querySelector('#amount')
-var currencies = []
-fetch('https://umar361.github.io/tasks/converter/data.json')
-    .then((response) => response.json())
-    .then((json) => {
-        // console.log(json);
-        // console.log(json["usd"])
-        currencies = json;
-        for (var currency in json) {
-            var opt = document.createElement('option');
-            opt.value = currency;
-            opt.innerHTML = json[currency].code +" - "+ json[currency].name;
-            selectTo.appendChild(opt);
-            var opt = document.createElement('option');
-            opt.value = currency;
-            opt.innerHTML = json[currency].code +" - "+ json[currency].name;
-            selectFrom.appendChild(opt);
-
-        }
-        for (var i = 0; i<json.length; i++){
-            
-        } 
-    });
-
-  
-
+const btn = document.querySelector(".btn");
+const selectFrom = document.querySelector("#select_from");
+const selectTo = document.querySelector("#select_to");
+const result = document.querySelector(".result");
+const result_par = document.querySelector(".result_par");
+let amount = document.querySelector("#amount");
+var currencies = [];
+fetch("https://umar361.github.io/tasks/converter/data.json")
+  .then((response) => response.json())
+  .then((json) => {
+    // console.log(json);
+    // console.log(json["usd"])
+    currencies = json;
+    for (var currency in json) {
+      var opt = document.createElement("option");
+      opt.value = currency;
+      opt.innerHTML = json[currency].code + " - " + json[currency].name;
+      selectTo.appendChild(opt);
+      var opt = document.createElement("option");
+      opt.value = currency;
+      opt.innerHTML = json[currency].code + " - " + json[currency].name;
+      selectFrom.appendChild(opt);
+    }
+  });
 
 const btnFunc = () => {
-    let base = selectFrom.value
-    let converTo = selectTo.value
-    let realAmount = amount.value
-    let fromCurrency = currencies[base]
-    let toCurrency = currencies[converTo]
+  let base = selectFrom.value;
+  let converTo = selectTo.value;
+  let realAmount = amount.value;
+  let fromCurrency = currencies[base];
+  let toCurrency = currencies[converTo];
 
-    const convert = () => {
-        return realAmount * toCurrency.rate
-    }
-    if (realAmount === '' ) {
-        console.log('Please enter amount');
-        result_par.innerText = 'Please Enter Amount'
-    }else if (base == "select" || converTo == "select" ) {
-        console.log('Currecnies not selected');
-        result_par.innerText = `Please select ${ converTo === base ? 'currencies ' : base === "select" ? 'source currency': 'destination currency'  } for conversion`
-    }
-    else if (base === converTo ) {
-        console.log('Same currencies selected');
-        result_par.innerText = 'Please select different currencies for conversion'
-    }
-    else {
-        result.innerHTML = `<button class="btn">Convert</button>
-        <h6 class="result_par">${realAmount} ${fromCurrency.code} is equal to <span class="result_span">${convert().toLocaleString()} ${toCurrency.code}</span></h6> 
-        <h6 class="result_par">Conversion Rate: <span class="result_span">${toCurrency.rate}</span></h6>
+  const convert = () => {
+    return realAmount * toCurrency.rate;
+  };
+  if (realAmount === "") {
+    console.log("Please enter amount");
+    result_par.innerText = "Please Enter Amount";
+  } else if (base == "select" || converTo == "select") {
+    console.log("Currecnies not selected");
+    result_par.innerText = `Please select ${
+      converTo === base
+        ? "currencies "
+        : base === "select"
+        ? "source currency"
+        : "destination currency"
+    } for conversion`;
+  } else if (base === converTo) {
+    console.log("Same currencies selected");
+    result_par.innerText = "Please select different currencies for conversion";
+  } else {
+    result.innerHTML = `<button class="btn">Convert</button>
+        <h6 class="result_par">${realAmount} ${
+      fromCurrency.code
+    } is equal to <span class="result_span">${convert().toLocaleString()} ${
+      toCurrency.code
+    }</span></h6> 
+        <h6 class="result_par">Conversion Rate: <span class="result_span">${
+          toCurrency.rate
+        }</span></h6>
         <h6 class="result_par">Conversion Time: <span class="result_span">${new Date().toUTCString()}</span></h6>
-        `
-    }
-}
+        `;
+  }
+};
 const func = (e) => {
-    if (e.keyCode == 13) {
-        btnFunc()
-    }
-}
-btn.addEventListener('click', btnFunc);
-amount.addEventListener('keypress', func)
-
-amount.addEventListener("keyup", () => {
-    if (!amount.value) {
-        result.innerHTML = `<button class="btn">Convert</button>`
-    }
-});
-
-const changer = (e) => {
-    let realAmount = amount.value
-    realAmount = e.target.value
-    btnFunc()
-}
-
-amount.addEventListener('input', changer)
-
-amount.oninput = function () {
-    if (this.value.length > 9) {
-        this.value = this.value.slice(0,9); 
-    }
-}
+  if (e.keyCode == 13) {
+    btnFunc();
+  }
+};
+btn.addEventListener("click", btnFunc);
+amount.addEventListener("keypress", func);
